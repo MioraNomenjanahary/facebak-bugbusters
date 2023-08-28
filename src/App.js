118ee86c5c26b0486f4bce57b5b99171ee7d11e9
MioraNomenjanahary <hei.miora.4@gmail.com>
2023-08-28 04:48:43 +0300

    Delete signupPages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './LoginPage/Login';
-import Signup from './SignupPage/Signup';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
       <Route index element= {<Login /> } />
           <Route path='/login' element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>   
   );
